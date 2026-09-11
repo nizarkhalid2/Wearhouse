9459aa42f79ab68d7efc46d71cd7c20f8f35da49
nizarkhalid2 <nizarkhalid321@gmail.com>
2026-09-11 18:39:50 +0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.WAREHOUSE_CONFIG = {
-  SUPABASE_URL: "https://smbbhxankojvpmistdct.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_HUdlBXrjT0DSqs8MHNIjog_H6Wsn3fr",
-  ADMIN_EMAIL: "admin@warehouse.com",
-  STORAGE_BUCKET: "product-images"
-};
